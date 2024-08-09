6c25bb4706f4fb4f95c4281ee006a494061f7075
Jave.Lin <linjf_008@126.com>
2024-08-09 15:21:09 +0800

    Update Testing.cs
    adding next frame call demo

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -13,6 +13,7 @@ public class Testing : MonoBehaviour
     public int no_arg_delaycall_timer_id = -1;
     public int no_arg_everyframecall_timer_id = -1;
     public int with_arg_intervalcall_timer_id = -1;
+    public int no_arg_nextframecall_timer_id = -1;
 
     public float with_time_scale_count;
     public float without_time_scale_count;
@@ -72,6 +73,12 @@ public class Testing : MonoBehaviour
         {
             TimerMgr<Player>.Inst.RemoveTimer(with_arg_intervalcall_timer_id);
             with_arg_intervalcall_timer_id = -1;
+        }
+
+        if (no_arg_nextframecall_timer_id != -1)
+        {
+            TimerMgr<Player>.Inst.RemoveTimer(no_arg_nextframecall_timer_id);
+            no_arg_nextframecall_timer_id = -1;
         }
     }
 
@@ -198,8 +205,9 @@ public class Testing : MonoBehaviour
     public void UnitTest()
     {
         _Testing_DelayCall();
-        _Testing_CallEveryFrame();
-        _Testing_CallInterval();
+        _Testing_EveryFrameCall();
+        _Testing_IntervalCall();
+        _Testing_NextFrameCall();
     }
 
     private void _Testing_DelayCall()
@@ -213,7 +221,7 @@ public class Testing : MonoBehaviour
             TimerMgr.Inst.DelayCall(() => { Debug.Log("This is my delay call testing, after 2 seonds."); }, 2f);
     }
 
-    private void _Testing_CallEveryFrame()
+    private void _Testing_EveryFrameCall()
     {
         if (no_arg_everyframecall_timer_id != -1)
         {
@@ -238,7 +246,7 @@ public class Testing : MonoBehaviour
 
     private Player player = new Player();
 
-    private void _Testing_CallInterval()
+    private void _Testing_IntervalCall()
     {
         if (with_arg_intervalcall_timer_id != -1)
         {
@@ -250,5 +258,58 @@ public class Testing : MonoBehaviour
             player.pos += Vector2.one;
             Debug.Log($"call interval, current player pos : {player.pos}");
         }, player, 1.5f);
+    }
+
+    private void _Testing_NextFrameCall()
+    {
+        // sync call
+        _refreshSyncCount = 0;
+        const int TIMES = 10;
+        for (int i = 0; i < TIMES; i++)
+        {
+            _RefreshBySync();
+        }
+        
+        // async call (next frame)
+        _refreshNextFrameCount = 0;
+        for (int i = 0; i < TIMES; i++)
+        {
+            if (no_arg_nextframecall_timer_id == -1)
+            {
+                no_arg_nextframecall_timer_id = TimerMgr.Inst.NextFrameCall(_RefreshByNextFrame);
+            }
+        }
+        
+        /*
+         * 输出：
+         *  _RefreshBySync call times : 1
+            _RefreshBySync call times : 2
+            _RefreshBySync call times : 3
+            _RefreshBySync call times : 4
+            _RefreshBySync call times : 5
+            _RefreshBySync call times : 6
+            _RefreshBySync call times : 7
+            _RefreshBySync call times : 8
+            _RefreshBySync call times : 9
+            _RefreshBySync call times : 10
+            
+            _RefreshByNextFrame call times : 1
+         */
+    }
+
+    private int _refreshSyncCount = 0;
+    private void _RefreshBySync()
+    {
+        ++_refreshSyncCount;
+        Debug.Log($"{nameof(_RefreshBySync)} call times : {_refreshSyncCount}");
+    }
+
+    private int _refreshNextFrameCount = 0;
+
+    private void _RefreshByNextFrame()
+    {
+        ++_refreshNextFrameCount;
+        Debug.Log($"{nameof(_RefreshByNextFrame)} call times : {_refreshNextFrameCount}");
+        no_arg_nextframecall_timer_id = -1;
     }
 }
